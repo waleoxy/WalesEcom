c1eb5104735a33633423579449177c9898388f56
Olawale Otubu <waleoxy@gmail.com>
2021-12-05 20:45:46 +0100

    layout components wrapper with header using mui

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
+import Layouts from "../components/Layouts";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Header</h1>
-      <h1>Row of list of featured products slide</h1>
-      <h1>Popular products grid</h1>
-      <h1>Footer</h1>
-    </div>
+    <Layouts>
+      <div>
+        <h1>Row of list of featured products slide</h1>
+        <h1>Popular products grid</h1>
+        <h1>Footer</h1>
+      </div>
+    </Layouts>
   );
 }
